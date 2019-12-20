24d4bd002b911eeadec4d7c701b837e58dad2e6d
Robert Hsu <roberthsu2003@gmail.com>
2019-12-20 14:44:22 +0800

    Update win2.py

--- pycharmProject/win2.py
+++ pycharmProject/win2.py
@@ -1,6 +1,18 @@
 from tkinter import *
 
+def openLight():
+    print("openLight")
+
+def closeLight():
+    print("close Light")
+
 if __name__ == "__main__":
     window = Tk()
     window.title("led control")
+    openButton = Button(window, text="開燈", command=openLight, padx = 10, pady = 10)
+    openButton.pack(side=LEFT)
+
+    closeButton = Button(window, text="關燈", command=closeLight, padx=10, pady=10)
+    closeButton.pack(side=LEFT)
+
     window.mainloop()
