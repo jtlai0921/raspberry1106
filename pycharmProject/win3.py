3118121113b868b3f81e219fd9f6bd00440b52db
Robert Hsu <roberthsu2003@gmail.com>
2020-01-03 14:54:23 +0800

    modify

--- pycharmProject/win3.py
+++ pycharmProject/win3.py
@@ -1,1 +1,21 @@
 #! usr/bin/python3
+
+from gpiozero import MCP3008
+from tkinter import Tk
+from time import sleep
+
+'''
+while True:
+    lm35 = MCP3008(channel=6)
+    print(lm35.value * 3.3 * 100)
+    sleep(1)
+'''
+class LM35App:
+    def __init__(self):
+        print("LM35App的實體被建立")
+
+if __name__ == "__main__":
+    window = Tk()
+    window.title("LM35")
+    app = LM35App()
+    window.mainloop()
