13df016524ab4ee152e25b0d560301772fc937b0
Robert Hsu <roberthsu2003@gmail.com>
2020-01-03 11:30:48 +0800

    modify

---
+++ pythonProject1/sample1.py
@@ -0,0 +1,9 @@
+#! usr/bin/python3
+class Person:
+    pass
+
+
+if __name__ == '__main__':
+    p1 = Person()
+    p2 = Person()
+    p3 = Person()
--- pythonProject1/weather1.py
+++ pythonProject1/weather1.py
@@ -1,4 +1,5 @@
 from boxes import daily,weekly
 
-print("Daily forecast:", daily.forecast())
+print("Daily forecast:", daily.forecast().split(' '))
 print("Weekly forecast:", weekly.forecast())
+type('week')
