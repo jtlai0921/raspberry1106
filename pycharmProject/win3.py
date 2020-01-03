54e2f14abb95a63235901994bfede16e7f00b221
Robert Hsu <roberthsu2003@gmail.com>
2020-01-03 15:36:23 +0800

    modify

--- pycharmProject/win3.py
+++ pycharmProject/win3.py
@@ -2,22 +2,21 @@
 
 from gpiozero import MCP3008
 from tkinter import Tk
-from time import sleep
+from threading import Timer
 
 class LM35App:
     def __init__(self):
         print("LM35App的實體被建立")
         self.lm35 = MCP3008(channel=6)
+        self.checkTemperature()
 
     def checkTemperature(self):
-        while True:
-            print(self.lm35.value * 3.3 * 100)
-        sleep(1)
+        print(self.lm35.value * 3.3 * 100)
+        Timer(1, self.checkTemperature()).start()
 
 
 if __name__ == "__main__":
     window = Tk()
     window.title("LM35")
     app = LM35App()
-    app.checkTemperature()
     window.mainloop()
