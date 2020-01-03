7d0fdfb00d5d637724ab727f06356dbe05b86bef
Robert Hsu <roberthsu2003@gmail.com>
2020-01-03 10:28:40 +0800

    modify

--- pythonProject1/weatherman.py
+++ pythonProject1/weatherman.py
@@ -1,6 +1,6 @@
-from report import get_description
+from report import get_description as get_desc
 import sys
 
-description = get_description()
+description = get_desc()
 print("Today's weather:", description)
 print(sys.path)
