2eb55c68758200bd2b91364fafd71fd4df3a9187
Robert Hsu <roberthsu2003@gmail.com>
2020-01-10 10:34:30 +0800

    Update obj1.py

--- pythonProject1/obj1.py
+++ pythonProject1/obj1.py
@@ -1,12 +1,16 @@
 class Person():
-
-
-    def __init__(self,name,tall):
-        print('初始化')
+    def __init__(self,name='robert',tall=100):
         self.name = name
         self.tall = tall
 
+
+
+
 if __name__ == '__main__':
-   someOne = Person('Alan',170)
-   anotherOne = Person("Jenny",160)
+   noOne = Person()
+   someOne = Person(name = "alen")
+   anotherOne = Person(name='jenny',tall = 170)
    print(someOne.name)
+   print(someOne.tall)
+   print(anotherOne.name)
+   print(anotherOne.tall)
