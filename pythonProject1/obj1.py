b9962b7be00e35fc94d1ea0e209e1e792ea583bd
Robert Hsu <roberthsu2003@gmail.com>
2020-01-10 11:18:06 +0800

    modify

--- pythonProject1/obj1.py
+++ pythonProject1/obj1.py
@@ -4,13 +4,28 @@ class Person():
         self.tall = tall
 
 
+class Car():
+    def __init__(self,name):
+        self.name = name
+
+    def exclaim(self):
+        print("I'm a Car")
+
+class Yugo(Car):
+    def __init__(self,name,weight):
+        super().__init__(name)
+        self.weight = weight
+
+    def exclaim(self): #override method
+        print("I'm a Yugo")
+
 
 
 if __name__ == '__main__':
-   noOne = Person()
-   someOne = Person(name = "alen")
-   anotherOne = Person(name='jenny',tall = 170)
-   print(someOne.name)
-   print(someOne.tall)
-   print(anotherOne.name)
-   print(anotherOne.tall)
+   give_me_a_car = Car('happyCar')
+   give_me_a_car.exclaim()
+   print('carName=', give_me_a_car.name)
+
+   give_me_a_yugo = Yugo('Happy Yugo',59)
+   give_me_a_yugo.exclaim()
+   print('yugoName=',give_me_a_yugo.name)
