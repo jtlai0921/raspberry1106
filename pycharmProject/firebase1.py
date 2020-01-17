75a7ba94f5184732cbd91a81964389b556175c44
Robert Hsu <roberthsu2003@gmail.com>
2020-01-17 11:57:11 +0800

    modify

--- pycharmProject/firebase1.py
+++ pycharmProject/firebase1.py
@@ -6,3 +6,8 @@ from firebase_admin import db
 
 if __name__ == '__main__':
     cred = credentials.Certificate('/home/pi/Documents/certificate/raspberryfirebase-firebase-adminsdk-y4f0x-65514e121f.json')
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://raspberryfirebase.firebaseio.com/'
+    })
+    ref = db.reference('raspberrypi/LED_Control')
+    print(ref.get())
