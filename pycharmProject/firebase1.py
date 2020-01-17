dea1eb33afa9e2fcce5563e1a77ddc86ba76b39c
Robert Hsu <roberthsu2003@gmail.com>
2020-01-17 14:00:02 +0800

    modify

--- pycharmProject/firebase1.py
+++ pycharmProject/firebase1.py
@@ -3,14 +3,19 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+from gpiozero import LED
+
+led = LED(25)
 
 def ledControlDataChange(even):
     print('內容改變')
     ledState = even.data['LED25']
     if ledState == 'OPEN':
         print('Open')
+        led.on()
     else:
         print('Close')
+        led.off()
 
 if __name__ == '__main__':
     cred = credentials.Certificate('/home/pi/Documents/certificate/raspberryfirebase-firebase-adminsdk-y4f0x-65514e121f.json')
