f9fc5bfcad67889acbce69d69ce69b1e8464fb90
Robert Hsu <roberthsu2003@gmail.com>
2020-01-17 11:40:39 +0800

    modify

--- pycharmProject/firebase1.py
+++ pycharmProject/firebase1.py
@@ -1,4 +1,8 @@
 #!/home/pi/Documents/pycharmProject/env01/bin/python3
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
 if __name__ == '__main__':
-    print('firebase')
+    cred = credentials.Certificate('/home/pi/Documents/certificate/raspberryfirebase-firebase-adminsdk-y4f0x-65514e121f.json')
