5e2cd3bd3020c182dc1ff1263472f0b87565b0ba
Robert Hsu <roberthsu2003@gmail.com>
2020-01-17 10:15:33 +0800

    Update class2.py

--- pythonProject1/class2.py
+++ pythonProject1/class2.py
@@ -3,16 +3,25 @@
 
 
 class Car:
+    def __init__(self, name):
+        self.name = name
+
     def exclaim(self):
-        print("I am a Car!")
+        print("I am a ", self.name)
 
 class Yugo(Car):
     #instance method override
     def exclaim(self):
         print("I am a Yugo!")
 
+    def need_a_push(self):
+        print('A little help here?')
+
 if __name__ == '__main__':
-    car = Car()
+    car = Car('blackCar')
+    print(car.name)
     car.exclaim()
-    myYugo = Yugo()
+    myYugo = Yugo('whiteYugo')
+    print(myYugo.name)
     myYugo.exclaim()
+    myYugo.need_a_push()
