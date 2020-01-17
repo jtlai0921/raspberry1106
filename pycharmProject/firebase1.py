aad17e97f57737e009c019b3dfc9f47a57e6ce2d
Robert Hsu <roberthsu2003@gmail.com>
2020-01-17 13:41:20 +0800

    modify

--- pycharmProject/firebase1.py
+++ pycharmProject/firebase1.py
@@ -4,10 +4,19 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
+def ledControlDataChange(even):
+    print('內容改變')
+    ledState = even.data['LED25']
+    if ledState == 'OPEN':
+        print('Open')
+    else:
+        print('Close')
+
 if __name__ == '__main__':
     cred = credentials.Certificate('/home/pi/Documents/certificate/raspberryfirebase-firebase-adminsdk-y4f0x-65514e121f.json')
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://raspberryfirebase.firebaseio.com/'
     })
-    ref = db.reference('raspberrypi/LED_Control')
-    print(ref.get())
+    ledControlRef = db.reference('raspberrypi/LED_Control')
+    ledControlRef.listen(ledControlDataChange)
+
